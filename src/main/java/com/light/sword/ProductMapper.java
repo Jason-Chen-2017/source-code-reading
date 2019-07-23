b3b7d31137d331cc174aef71db23840c98693d97
jack <guangjian.cgj@alibaba-inc.com>
2019-07-24 02:15:32 +0800

    anno demo

--- src/main/java/com/light/sword/ProductMapper.java
+++ src/main/java/com/light/sword/ProductMapper.java
@@ -1,16 +1,12 @@
 package com.light.sword;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.ResultType;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
-@Mapper
 public interface ProductMapper {
 
     @Select("select * from product")
-    @ResultType(Product.class)
     List<Product> findAll();
 
 }
